6c9c8fbbcf1e6a3e2f0d93d7e38a90c2456132c4
xiaoqiu <18324465803@163.com>
2019-07-03 16:50:14 +0800

    svg随机数据调整

--- src/components/svgbg.vue
+++ src/components/svgbg.vue
@@ -1,6 +1,6 @@
 <template>
   <svg id="mysvg" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" >
-    <text :x='posx' :y="posY" transform="rotate(20 10,20)" v-for="item in items">{{item}}</text>
+    <text :x='item.posX' :y='item.posY' v-for="item in items">{{item.text}}</text>
     <polyline points="23,329 90,328 63,413" fill="none" stroke="black" />
   </svg>
 </template>
@@ -10,26 +10,27 @@ export default {
   name: 'svgbg',
   data: function(){
     return {
-      posX: 20,
-      posY: 90,
       items: [
-        'Function', 'var', 'Window', 'Math.round', 'style', 'Background', 'width', 'Height', 'Methods', 'random', 'angular', 'float', 'border', 'css', 'javascript', 'php', 'vue',
-        'react', 'install', 'flower', 'Header', 'select', 'input', 'node', 'Lovely', 'Luky', 'Goddess', 'three', 'sweet', 'study', 'Music','margin','Tomorrow will be better!',
-        'transfrom', 'If(Happy){Smile}else{Cry}', 'Don\'t cry', 'You are so beautiful', 'Good Girl', 'You are not alone', 'GoodBye', 'Sunshine', 'Strong', 'You will success', 
-        'polyfill', 'merge', 'Newyork', 'Paris']
+        {"text":"Function","posX":100,"posY":80},{"text":"var","posX":110,"posY":109},{"text":"Window","posX":90,"posY":96},{"text":"Math.round","posX":160,"posY":100},
+        {"text":"style","posX":200,"posY":20},{"text":"Background","posX":20,"posY":30},{"text":"width","posX":20,"posY":30},{"text":"Height","posX":20,"posY":30},
+        {"text":"Methods","posX":20,"posY":30},{"text":"random","posX":20,"posY":30},{"text":"angular","posX":20,"posY":30},{"text":"float","posX":20,"posY":30},
+        {"text":"border","posX":20,"posY":30},{"text":"css","posX":20,"posY":30},{"text":"javascript","posX":20,"posY":30},{"text":"php","posX":20,"posY":30},
+        {"text":"Vue","posX":20,"posY":30},{"text":"react","posX":20,"posY":30},{"text":"install","posX":20,"posY":30},{"text":"flower","posX":20,"posY":30},
+        {"text":"Header","posX":20,"posY":30},{"text":"select","posX":20,"posY":30},{"text":"input","posX":20,"posY":30},{"text":"node","posX":20,"posY":30},
+        {"text":"Lovely","posX":20,"posY":30},{"text":"study","posX":20,"posY":30},{"text":"Goddess","posX":20,"posY":30},{"text":"three","posX":20,"posY":30},
+        {"text":"sweet","posX":20,"posY":30},{"text":"Music","posX":20,"posY":30},{"text":"margin","posX":20,"posY":30},{"text":"Tomorrow will be better!","posX":20,"posY":30},
+        {"text":"transfrom","posX":20,"posY":30},{"text":"If(Happy){Smile}else{Cry}","posX":20,"posY":30},{"text":"'Don\'t cry","posX":20,"posY":30},
+        {"text":"Good Girl","posX":20,"posY":30},{"text":"You are not alone","posX":20,"posY":30},{"text":"GoodBye","posX":20,"posY":30},{"text":"Sunshine","posX":20,"posY":30},
+        {"text":"Strong","posX":20,"posY":30},{"text":"You will success","posY":20,"posY":30},{"text":"polyfill","posX":20,"posY":30},{"text":"merge","posX":20,"posY":30},
+        {"text":"Newyork","posX":20,"posY":30},{"text":"Paris","posX":20,"posY":30},{"text":"You are so beautiful","posX":20,"posY":30}
+      ]
       
     }
   },
-  methods: {
-    setPos:function(){
-        var num1 = Math.random()*100;/*产生随机数*/
-        var num2 = Math.random()*80;
-        console.log(num1,num2)
-        return this.posX=num1,this.posY=num2;
-    }
+  computed:{
+    textPos:function(){console.log(this)}
   },
   mounted: function () {
-    this.setPos();
     
   }
 }
--- src/main.js
+++ src/main.js
@@ -16,10 +16,9 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: '#app', 
   router,
   components: { App },
   template: '<App/>',
   render:h=>h(App)
 })
-
