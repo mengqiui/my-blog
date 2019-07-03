25a2370065d01b1185734c691a54df3c760b8ac0
xiaoqiu <18324465803@163.com>
2019-07-03 17:47:40 +0800

    svg随机数初步实现

--- src/components/svgbg.vue
+++ src/components/svgbg.vue
@@ -1,6 +1,6 @@
 <template>
   <svg id="mysvg" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" >
-    <text :x='item.posX' :y='item.posY' v-for="item in items">{{item.text}}</text>
+    <text :x='item.posX' :y='item.posY' v-for="item in svglist">{{item.text}}</text>
     <polyline points="23,329 90,328 63,413" fill="none" stroke="black" />
   </svg>
 </template>
@@ -10,7 +10,7 @@ export default {
   name: 'svgbg',
   data: function(){
     return {
-      items: [
+      svglist: [
         {"text":"Function","posX":100,"posY":80},{"text":"var","posX":110,"posY":109},{"text":"Window","posX":90,"posY":96},{"text":"Math.round","posX":160,"posY":100},
         {"text":"style","posX":200,"posY":20},{"text":"Background","posX":20,"posY":30},{"text":"width","posX":20,"posY":30},{"text":"Height","posX":20,"posY":30},
         {"text":"Methods","posX":20,"posY":30},{"text":"random","posX":20,"posY":30},{"text":"angular","posX":20,"posY":30},{"text":"float","posX":20,"posY":30},
@@ -21,17 +21,25 @@ export default {
         {"text":"sweet","posX":20,"posY":30},{"text":"Music","posX":20,"posY":30},{"text":"margin","posX":20,"posY":30},{"text":"Tomorrow will be better!","posX":20,"posY":30},
         {"text":"transfrom","posX":20,"posY":30},{"text":"If(Happy){Smile}else{Cry}","posX":20,"posY":30},{"text":"'Don\'t cry","posX":20,"posY":30},
         {"text":"Good Girl","posX":20,"posY":30},{"text":"You are not alone","posX":20,"posY":30},{"text":"GoodBye","posX":20,"posY":30},{"text":"Sunshine","posX":20,"posY":30},
-        {"text":"Strong","posX":20,"posY":30},{"text":"You will success","posY":20,"posY":30},{"text":"polyfill","posX":20,"posY":30},{"text":"merge","posX":20,"posY":30},
+        {"text":"Strong","posX":20,"posY":30},{"text":"You will success","posX":20,"posY":20,"posY":30},{"text":"polyfill","posX":20,"posY":30},{"text":"merge","posX":20,"posY":30},
         {"text":"Newyork","posX":20,"posY":30},{"text":"Paris","posX":20,"posY":30},{"text":"You are so beautiful","posX":20,"posY":30}
       ]
       
     }
   },
   computed:{
-    textPos:function(){console.log(this)}
+    textPos:function(){
+      var randomX = Math.floor((Math.random()*500)+20);
+      for(var key in this.svglist){
+        //console.log(this.svglist[key].posX,this.svglist[key].posY)
+        console.log(Math.floor((Math.random()*500)+20));
+        this.svglist[key].posX =  Math.floor((Math.random()*1800)+20);
+        this.svglist[key].posY =  Math.floor((Math.random()*800)+80);
+      }
+    }
   },
   mounted: function () {
-    
+    this.textPos
   }
 }
 
