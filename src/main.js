cc53a74eb3e7e5e1a93ddace287d324b4ee13e4f
xiaoqiu <18324465803@163.com>
2019-07-11 17:44:27 +0800

    首页主内容区域设计完成

--- src/main.js
+++ src/main.js
@@ -39,3 +39,6 @@ router.beforeEach((to,from,next)=>{
   }
   next()
 })
+
+
+
--- src/views/index.vue
+++ src/views/index.vue
@@ -18,21 +18,75 @@
           </el-card>
         </el-col>
       </el-row>
-      <el-row :gutter="12">
+      <el-row :gutter="12" style="margin-top:20px">
         <el-col :span="24">
-          <div class="clearfix">
+          <!-- <div class="clearfix">
             <el-divider content-position="left"><i class="el-icon-sugar"></i>吧唧吧唧</el-divider>
+          </div> -->
+          <!-- 该模块设计参考：http://demo.cssmoban.com/cssthemes6/tpmo_527_sided/index.html -->
+          <div class="index-content"> <!--总背景色-->
+            <div class="index-content-middle">  <!-- 模块居中 -->
+                <!-- 第一块：内容 -->
+              <div class="content-item">
+                <el-card shadow="hover" class="item-box">
+                  <el-image :src="imgSrc[0]" class="imgfill" lazy>
+                    <div slot="error" class="image-slot">
+                      <i class="el-icon-picture-outline"></i>
+                    </div>
+                  </el-image>
+                  <div class="textlist">
+                    <div class="texttitle">
+                      <h4>文章标题</h4>
+                      <a href="javascript:void(0)">我是文本文本文本文本，图片容器，在保留原生img的特性下，支持懒加载，自定义占位、加载失败等，图片容器，在保留原生img的特性下，支持懒加载，自定义占位、加载失败等，图片容器，在保留原生img的特性下，支持懒加载，自定义占位、加载失败等</a>
+                    </div>
+                    <div><el-tag size="mini">css3</el-tag><el-tag type="success" size="mini">前端</el-tag><el-tag type="info" size="mini">javascript</el-tag></div>
+                    <p class="infobox">
+                      <i class="el-icon-time"></i><span>发布于 2019-07-11</span>
+                      <i class="el-icon-view"></i><span>1425</span>
+                      <i class="el-icon-chicken"><span>352</span></i>
+                    </p>
+                  </div>
+                </el-card>
+              </div>
+                <!-- 第二块：内容 -->
+              <div class="content-item">
+                <el-card shadow="hover" class="item-box">
+                  <div class="textlist">
+                    <h4>文章标题</h4>
+                    <a href="javascript:void(0)">我是文本文本文本文本，图片容器，在保留原生img的特性下，支持懒加载，自定义占位、加载失败等</a>
+                    <div><el-tag size="mini">css3</el-tag><el-tag type="success" size="mini">前端</el-tag><el-tag type="info" size="mini">javascript</el-tag></div>
+                  </div>
+                  <el-image :src="imgSrc[1]" class="imgfill">
+                    <div slot="error" class="image-slot">
+                      <i class="el-icon-picture-outline"></i>
+                    </div>
+                  </el-image>
+                </el-card>
+              </div>
+                <!-- 第三块：内容 -->
+              <div class="content-item">
+                <el-card shadow="hover" class="item-box">
+                  <el-image :src="imgSrc[0]" class="imgfill">
+                    <div slot="error" class="image-slot">
+                      <i class="el-icon-picture-outline"></i>
+                    </div>
+                  </el-image>
+                  <div class="textlist">
+                    <h4>文章标题</h4>
+                    <a href="javascript:void(0)">我是文本文本文本文本，图片容器，在保留原生img的特性下，支持懒加载，自定义占位、加载失败等</a>
+                    <div><el-tag size="mini">css3</el-tag><el-tag type="success" size="mini">前端</el-tag><el-tag type="info" size="mini">javascript</el-tag></div>
+                  </div>
+                </el-card>
+              </div>
+            </div>
           </div>
-          <el-card shadow="hover" class="index-main-content">
-            <div class="content-item">我是list内容</div>
-          </el-card>
         </el-col>
       </el-row>
       </el-main>
-      <el-aside class="main-info">
+      <el-card shadow="hover" class="main-info">
         <!-- 城市定位 气候获取 -->
         <div v-html="locationHtml">{{locationHtml}}</div>
-      </el-aside>
+      </el-card>
     </el-container>
   </section>
 </template>
@@ -48,7 +102,8 @@ export default {
       locationHtml:"",
       noticeanimate:false,
       noticeList:'',
-      timer:null
+      timer:null,
+      imgSrc:["../../static/img/index01.jpg","../../static/img/index02.jpg"]
     }
   },
   methods: {
@@ -63,7 +118,7 @@ export default {
       // https://www.tianqiapi.com/?action=v6   天气API接口文档
 
       this.$axios.get(this.HOST + '/api.php',{params:{style:'ts',skin:'gif',ip:returnCitySN["cip"]?returnCitySN["cip"]:"124.160.26.254",column:1
-      }}).then(res=>{that.locationHtml=res.data?res.data:"网络加载异常···";
+      }}).then(res=>{that.locationHtml=res.data;
       }).catch(err=>{console.log(err)})
   
     },
